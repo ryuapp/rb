2bda8cabb16b4cf06e6b787a511b3e840a5c38ae
ryu <114303361+ryuapp@users.noreply.github.com>
2025-01-29 02:16:36 +0900

    v0.1.0

---
+++ release.ts
@@ -0,0 +1,48 @@
+import { crypto } from "@std/crypto";
+import zonToJson from "z2j";
+
+function loadBuildZigZon() {
+  const zon = zonToJson(Deno.readTextFileSync("build.zig.zon"));
+  return JSON.parse(zon);
+}
+
+const zip = new Deno.Command("powershell", {
+  args: [
+    "Compress-Archive -Path zig-out/bin/rb.exe -DestinationPath dist/rb-x86_64-pc-windows-msvc.zip -Force",
+  ],
+});
+
+zip.spawn();
+const buildZigZon = loadBuildZigZon();
+const hash = await crypto.subtle.digest(
+  "SHA-256",
+  Deno.readFileSync("dist/rb-x86_64-pc-windows-msvc.zip"),
+);
+
+const scoopTemplate = {
+  version: buildZigZon.version,
+  homepage: "https://github.com/ryuapp/rb",
+  license: "MIT",
+  architecture: {
+    "64bit": {
+      url:
+        `https://github.com/ryuapp/rb/releases/download/v${buildZigZon.version}/rb-x86_64-pc-windows-msvc.zip`,
+      hash: hash,
+    },
+  },
+  bin: "rb.exe",
+  checkver: "github",
+  autoupdate: {
+    architecture: {
+      "64bit": {
+        url:
+          "https://github.com/ryuapp/rb/releases/download/v$version/rb-x86_64-pc-windows-msvc.zip",
+      },
+    },
+  },
+};
+
+Deno.writeFileSync(
+  "rb.json",
+  new TextEncoder().encode(JSON.stringify(scoopTemplate, null, 2) + "\n"),
+);
