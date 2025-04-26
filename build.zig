4ae2b4c6ef7b1598562da2b7a8f44f480e94349a
ryu <114303361+ryuapp@users.noreply.github.com>
2025-04-26 15:16:57 +0900

    fix memory leak in trash (#4)

--- build.zig
+++ build.zig
@@ -4,6 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const test_step = b.step("test", "Run all tests");
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_tests = b.addRunArtifact(tests);
+    test_step.dependOn(&run_tests.step);
+
     const clap = b.dependency("clap", .{});
     const exe = b.addExecutable(.{
         .name = "rb",
@@ -22,6 +31,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the cli");
     run_step.dependOn(&run_cmd.step);
 }
